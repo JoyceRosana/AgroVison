a0947c4c962b42b2f148932133c5a10df6e3ba08
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-06 19:36:29 -0300

    Update sobre.js

---
+++ modulos/sobre/sobre.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnContato = document.getElementById("btnContato");
+
+    if (btnContato) {
+        btnContato.addEventListener("click", () => {
+
+            const email = "tavares.rosana@escola.pr.gov.br";
+            const assunto = encodeURIComponent("Contato sobre o site AgroTech Sustentável");
+
+const mensagem = encodeURIComponent(
+`Olá,
+
+Estou entrando em contato sobre o site AgroTech Sustentável.
+
+Mensagem:
+`
+);
+
+           window.open(
+`mailto:${email}?subject=${assunto}&body=${mensagem}`,
+"_blank"
+           )
+        });
+    }
+});
