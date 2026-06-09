4ba5018ce6e51ae186a3e3405b1fb8e3cd6b847a
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-09 12:32:33 -0300

    css, js e html

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -154,32 +154,21 @@ console.log("CULTURA:", culturaSelecionada);
 // ==========================================
 if (status === "vazio" && culturaSelecionada) {
   const cultura = culturas[culturaSelecionada];
-const planta = document.createElement("img");
-planta.className = "planta";
-planta.src = cultura.broto;
+  const planta = terra.querySelector(".planta");
+  planta.src = cultura.broto;
+  planta.classList.remove("oculto");
 
-// insere a planta dentro da célula
-terra.appendChild(planta);
+  terra.dataset.status = "crescendo";
+  terra.dataset.cultura = culturaSelecionada;
 
-// centraliza no meio da imagem da terra
-planta.style.position = "absolute";
-planta.style.left = "50%";
-planta.style.top = "50%";
-planta.style.transform = "translate(-50%, -55%)"; // leve ajuste para compensar perspectiva
-planta.style.zIndex = 10; // planta acima da terra
+  setTimeout(() => {
+    planta.src = cultura.jovem;
+  }, cultura.crescimento / 2);
 
-terra.dataset.status = "crescendo";
-terra.dataset.cultura = culturaSelecionada;
-terra.planta = planta;
-
-setTimeout(() => {
-  planta.src = cultura.jovem;
-}, cultura.crescimento / 2);
-
-setTimeout(() => {
-  planta.src = cultura.pronta;
-  terra.dataset.status = "pronto";
-}, cultura.crescimento);
+  setTimeout(() => {
+    planta.src = cultura.pronta;
+    terra.dataset.status = "pronto";
+  }, cultura.crescimento);
 }
     return;
 }
