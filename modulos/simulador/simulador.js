46d6668c4e125b2b458214e214f82f6c55788411
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-09 12:09:26 -0300

    js

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -161,12 +161,14 @@ planta.src = cultura.broto;
 // insere a planta dentro da célula
 terra.appendChild(planta);
 
-// centraliza no meio da imagem da terra
+// centraliza no centro da terra
 planta.style.position = "absolute";
 planta.style.left = "50%";
 planta.style.top = "50%";
-planta.style.transform = "translate(-50%, -55%)"; // leve ajuste para compensar perspectiva
-planta.style.zIndex = 5; // garante que a planta fique acima da terra
+planta.style.transform = "translate(-50%, -50%)";
+
+// garante que a planta fique acima da terra
+planta.style.zIndex = 10;
 
 terra.dataset.status = "crescendo";
 terra.dataset.cultura = culturaSelecionada;
