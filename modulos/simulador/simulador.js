88439372f03c4021d4a590b40f54e66789f47b8f
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-09 11:37:12 -0300

    js

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -165,7 +165,7 @@ if (status === "vazio" && culturaSelecionada) {
   planta.style.position = "absolute";
   planta.style.left = "50%";
   planta.style.top = "50%";
-  planta.style.transform = "translate(-30%, -70%)";
+  planta.style.transform = "translate(-50%, -60%)";
 
   terra.dataset.status = "crescendo";
   terra.dataset.cultura = culturaSelecionada;
