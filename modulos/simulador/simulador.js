0c2684a26e0571b274e80a811be5c6e6eea173d5
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-09 04:57:22 -0300

    css e js

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -125,7 +125,8 @@ terra.addEventListener("click", (e) => {
   console.log("CLIQUE NA TERRA");
 
   const status = terra.dataset.status;
-  
+  onsole.log("BOTÃO CLICADO");
+console.log(botao.dataset.cultura);
   console.log("STATUS:", status);
   console.log("CULTURA:", culturaSelecionada);
 
