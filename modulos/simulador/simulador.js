45417307265f61809299a248d9c9a6bb8cc862a3
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-10 19:34:58 -0300

    general update

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -4,7 +4,13 @@ const plantacao = document.getElementById("plantacao");
 const menuPlantio = document.getElementById("menuPlantio");
 const cursorPlantio = document.getElementById("cursorPlantio");
 const cursorFoice = document.getElementById("cursorFoice");
+const balaoFoice = 
+document.getElementById("balaoFoice");
 
+const foiceBalao =
+document.getElementById("foiceBalao");
+
+let terraSelecionada = null;
 let culturaSelecionada = null;
 let modoColheita = false;
 
@@ -59,65 +65,139 @@ viewport.addEventListener("mousemove", e => {
 
 const culturas = {
 
-milho:{
+    milho:{
 
-    crescimento:15000,
+        crescimento:15000,
 
-    broto:"/img/simulador/milho_broto.png",
-    jovem:"/img/simulador/milho_jovem.png",
-    pronta:"/img/simulador/milho_adulto.png",
+        broto:"/img/simulador/milho_broto.png",
+        jovem:"/img/simulador/milho_jovem.png",
+        pronta:"/img/simulador/milho_adulto.png",
 
-    brotoWidth:"60px",
-    brotoHeight:"80px",
-     brotoWidth:"60px",
-    brotoHeight:"80px",
+        brotoWidth:"80px",
+        brotoHeight:"90px",
 
-    jovemWidth:"120px",
-    jovemHeight:"160px",
+        jovemWidth:"120px",
+        jovemHeight:"140px",
 
-    adultoWidth:"150px",
-    adultoHeight:"220px",
+        adultoWidth:"150px",
+        adultoHeight:"180px",
 
-    brotoX:"-10px",
-    brotoY:"0px",
+        brotoX:"-12px",
+        brotoY:"0px",
 
-    jovemX:"0px",
-    jovemY:"0px",
+        jovemX:"0px",
+        jovemY:"0px",
 
-    adultoX:"-12px",
-    adultoY:"5px"
-},
-
-    soja: {
-        crescimento: 10000,
-        broto: "/img/simulador/soja_broto.png",
-        jovem: "/img/simulador/soja_jovem.png",
-        pronta: "/img/simulador/soja_pronto.png",
-        largura: "100px"
+        adultoX:"-10px",
+        adultoY:"0px"
     },
 
-    alface: {
-        crescimento: 8000,
-        broto: "/img/simulador/alface_broto.png",
-        jovem: "/img/simulador/alface_jovem.png",
-        pronta: "/img/simulador/alface_pronto.png",
-        largura: "80px"
+    soja:{
+
+        crescimento:10000,
+
+        broto:"/img/simulador/soja_broto.png",
+        jovem:"/img/simulador/soja_jovem.png",
+        pronta:"/img/simulador/soja_pronto.png",
+
+        brotoWidth:"60px",
+        brotoHeight:"60px",
+
+        jovemWidth:"90px",
+        jovemHeight:"90px",
+
+        adultoWidth:"110px",
+        adultoHeight:"110px",
+
+        brotoX:"0px",
+        brotoY:"0px",
+
+        jovemX:"0px",
+        jovemY:"0px",
+
+        adultoX:"0px",
+        adultoY:"0px"
     },
 
-    cenoura: {
-        crescimento: 9000,
-        broto: "/img/simulador/cenoura_broto.png",
-        jovem: "/img/simulador/cenoura_jovem.png",
-        pronta: "/img/simulador/cenoura_pronto.png",
-        largura: "90px"
+    alface:{
+
+        crescimento:8000,
+
+        broto:"/img/simulador/alface_broto.png",
+        jovem:"/img/simulador/alface_jovem.png",
+        pronta:"/img/simulador/alface_pronto.png",
+
+        brotoWidth:"50px",
+        brotoHeight:"50px",
+
+        jovemWidth:"70px",
+        jovemHeight:"70px",
+
+        adultoWidth:"90px",
+        adultoHeight:"90px",
+
+        brotoX:"0px",
+        brotoY:"0px",
+
+        jovemX:"0px",
+        jovemY:"0px",
+
+        adultoX:"0px",
+        adultoY:"0px"
     },
 
-    tomate: {
-        crescimento: 12000,
-        broto: "/img/simulador/tomate_broto.png",
-        jovem: "/img/simulador/tomate_jovem.png",
-        pronta: "/img/simulador/tomate_pronto.png",
-        largura: "120px"
+    cenoura:{
+
+        crescimento:9000,
+
+        broto:"/img/simulador/cenoura_broto.png",
+        jovem:"/img/simulador/cenoura_jovem.png",
+        pronta:"/img/simulador/cenoura_pronto.png",
+
+        brotoWidth:"50px",
+        brotoHeight:"60px",
+
+        jovemWidth:"70px",
+        jovemHeight:"80px",
+
+        adultoWidth:"90px",
+        adultoHeight:"100px",
+
+        brotoX:"0px",
+        brotoY:"0px",
+
+        jovemX:"0px",
+        jovemY:"0px",
+
+        adultoX:"0px",
+        adultoY:"0px"
+    },
+
+    tomate:{
+
+        crescimento:12000,
+
+        broto:"/img/simulador/tomate_broto.png",
+        jovem:"/img/simulador/tomate_jovem.png",
+        pronta:"/img/simulador/tomate_pronto.png",
+
+        brotoWidth:"70px",
+        brotoHeight:"80px",
+
+        jovemWidth:"100px",
+        jovemHeight:"120px",
+
+        adultoWidth:"130px",
+        adultoHeight:"150px",
+
+        brotoX:"0px",
+        brotoY:"0px",
+
+        jovemX:"0px",
+        jovemY:"0px",
+
+        adultoX:"0px",
+        adultoY:"0px"
     }
 
 };
@@ -132,9 +212,9 @@ const LINHAS = 10;
 const LARGURA = 50;
 const ALTURA = 20;
 
-for (let lin = 0; lin < LINHAS; lin++) {
+for(let lin=0; lin<LINHAS; lin++){
 
-    for (let col = 0; col < COLUNAS; col++) {
+    for(let col=0; col<COLUNAS; col++){
 
         const terra = document.createElement("div");
 
@@ -153,6 +233,7 @@ for (let lin = 0; lin < LINHAS; lin++) {
         planta.className = "planta oculto";
 
         terra.appendChild(imgTerra);
+
         terra.appendChild(planta);
 
         const posX =
@@ -164,103 +245,14 @@ for (let lin = 0; lin < LINHAS; lin++) {
             (lin * ALTURA);
 
         terra.style.left = posX + "px";
+
         terra.style.top = posY + "px";
 
         plantacao.appendChild(terra);
+
     }
+
 }
-
-/* ==================================
-   CURSOR
-================================== */
-
-plantacao.addEventListener("mouseenter", () => {
-
-    if (culturaSelecionada) {
-
-        cursorPlantio.classList.remove("oculto");
-
-    }
-
-});
-
-plantacao.addEventListener("mouseleave",()=>{
-
-    cursorPlantio.classList.add("oculto");
-
-    cursorFoice.classList.add("oculto");
-
-    document.body.classList.remove(
-        "plantio-ativo"
-    );
-
-    document.body.classList.remove(
-        "colheita-ativa"
-    );
-
-    culturaSelecionada = null;
-
-    modoColheita = false;
-
-});
-
-/* ==================================
-   ABRIR MENU
-================================== */
-
-plantacao.addEventListener("click", (e) => {
-
-    const terra = e.target.closest(".terra");
-
-    if(!terra) return;
-
-    /*
-       TERRA VAZIA
-    */
-
-    if(
-        terra.dataset.status === "vazio" &&
-        !culturaSelecionada &&
-        !modoColheita
-    ){
-
-        menuPlantio.style.left =
-            e.pageX + "px";
-
-        menuPlantio.style.top =
-            e.pageY + "px";
-
-        menuPlantio.classList.remove("oculto");
-
-        return;
-    }
-
-    /*
-       PLANTA PRONTA
-    */
-
-    if(
-        terra.dataset.status === "pronto" &&
-        !culturaSelecionada
-    ){
-
-        const resposta =
-            confirm("Deseja colher?");
-
-        if(!resposta) return;
-
-        modoColheita = true;
-
-        document.body.classList.add(
-            "colheita-ativa"
-        );
-
-        cursorFoice.classList.remove("oculto");
-
-        return;
-    }
-
-});
 /* ==================================
    ESCOLHER CULTURA
 ================================== */
@@ -274,9 +266,19 @@ document
         culturaSelecionada =
             botao.dataset.cultura;
 
+        modoColheita = false;
+
         menuPlantio.classList.add("oculto");
 
-        document.body.classList.add("plantio-ativo");
+        cursorFoice.classList.add("oculto");
+
+        document.body.classList.remove(
+            "colheita-ativa"
+        );
+
+        document.body.classList.add(
+            "plantio-ativo"
+        );
 
         cursorPlantio.classList.remove("oculto");
 
@@ -285,29 +287,61 @@ document
 });
 
 /* ==================================
+   PEGAR FOICE
+================================== */
+
+foiceBalao.addEventListener("click",()=>{
+
+    balaoFoice.classList.add("oculto");
+
+    modoColheita = true;
+
+    culturaSelecionada = null;
+
+    cursorPlantio.classList.add("oculto");
+
+    document.body.classList.remove(
+        "plantio-ativo"
+    );
+
+    cursorFoice.classList.remove("oculto");
+
+    document.body.classList.add(
+        "colheita-ativa"
+    );
+
+});
+
+/* ==================================
    PLANTAR
 ================================== */
 
-function plantar(terra) {
+function plantar(terra){
 
-    if (terra.dataset.status !== "vazio") return;
+    if(terra.dataset.status !== "vazio") return;
 
-    const cultura = culturas[culturaSelecionada];
+    const cultura =
+        culturas[culturaSelecionada];
 
-    const planta = terra.querySelector(".planta");
+    const planta =
+        terra.querySelector(".planta");
 
     planta.src = cultura.broto;
-planta.style.width = cultura.brotoWidth;
-planta.style.height = cultura.brotoHeight;
-planta.style.marginLeft = cultura.brotoX;
-planta.style.marginBottom = cultura.brotoY;
 
+    planta.style.width =
+        cultura.brotoWidth;
 
-planta.dataset.cultura = culturaSelecionada;
+    planta.style.height =
+        cultura.brotoHeight;
 
-    planta.style.width = cultura.largura;
+    planta.style.marginLeft =
+        cultura.brotoX;
+
+    planta.style.marginBottom =
+        cultura.brotoY;
 
     planta.style.left = "50%";
+
     planta.style.bottom = "20px";
 
     planta.style.transform =
@@ -315,35 +349,50 @@ planta.dataset.cultura = culturaSelecionada;
 
     planta.classList.remove("oculto");
 
-    terra.dataset.status = "crescendo";
+    terra.dataset.status =
+        "crescendo";
 
     terra.dataset.cultura =
         culturaSelecionada;
 
-    setTimeout(() => {
+    setTimeout(()=>{
 
-planta.src = cultura.jovem;
-planta.style.width = cultura.jovemWidth;
-planta.style.height = cultura.jovemHeight;
-planta.style.marginLeft = cultura.jovemX;
-planta.style.marginBottom = cultura.jovemY;        
+        planta.src =
+            cultura.jovem;
 
+        planta.style.width =
+            cultura.jovemWidth;
 
-planta.classList.add("milho-fixo");
+        planta.style.height =
+            cultura.jovemHeight;
+
+        planta.style.marginLeft =
+            cultura.jovemX;
+
+        planta.style.marginBottom =
+            cultura.jovemY;
 
     }, cultura.crescimento / 2);
 
-    setTimeout(() => {
+    setTimeout(()=>{
 
-        planta.src = cultura.pronta;
-planta.style.width = cultura.adultoWidth;
-planta.style.height = cultura.adultoHeight;
-planta.style.marginLeft = cultura.adultoX;
-planta.style.marginBottom = cultura.adultoY;
+        planta.src =
+            cultura.pronta;
 
-planta.classList.add("milho-fixo");
+        planta.style.width =
+            cultura.adultoWidth;
 
-        terra.dataset.status = "pronto";
+        planta.style.height =
+            cultura.adultoHeight;
+
+        planta.style.marginLeft =
+            cultura.adultoX;
+
+        planta.style.marginBottom =
+            cultura.adultoY;
+
+        terra.dataset.status =
+            "pronto";
 
     }, cultura.crescimento);
 
@@ -353,14 +402,14 @@ planta.classList.add("milho-fixo");
    COLHER
 ================================== */
 
-function colher(terra) {
+function colher(terra){
 
     const planta =
         terra.querySelector(".planta");
 
     planta.classList.add("colhendo");
 
-    setTimeout(() => {
+    setTimeout(()=>{
 
         planta.classList.remove("colhendo");
 
@@ -372,24 +421,32 @@ function colher(terra) {
 
         terra.dataset.cultura = "";
 
-        terra.dataset.cultura = "";
+        modoColheita = false;
 
         cursorFoice.classList.add("oculto");
-        document.body.classList.remove("colheita-ativa");
 
-    }, 300);
+        document.body.classList.remove(
+            "colheita-ativa"
+        );
+
+    },300);
 
 }
 
 /* ==================================
-   PLANTIO E COLHEITA CONTÍNUOS
+   PLANTIO E COLHEITA
 ================================== */
 
 plantacao.addEventListener("mouseover",(e)=>{
 
-    const terra = e.target.closest(".terra");
+    const terra =
+        e.target.closest(".terra");
 
     if(!terra) return;
+
+    /*
+       PLANTAR
+    */
 
     if(
         culturaSelecionada &&
@@ -397,7 +454,13 @@ plantacao.addEventListener("mouseover",(e)=>{
     ){
 
         plantar(terra);
+
+        return;
     }
+
+    /*
+       COLHER
+    */
 
     if(
         modoColheita &&
@@ -405,6 +468,7 @@ plantacao.addEventListener("mouseover",(e)=>{
     ){
 
         colher(terra);
+
     }
 
 });
@@ -413,24 +477,7 @@ plantacao.addEventListener("mouseover",(e)=>{
    CURSORES
 ================================== */
 
-document.addEventListener("mousemove", e => {
-
-  plantacao.addEventListener("mousemove", (e) => {
-
-    const terra = e.target.closest(".terra");
-
-    if(!terra) return;
-
-    if(
-        terra.dataset.status === "pronto" &&
-        !cursorFoice.classList.contains("oculto")
-    ){
-
-        colher(terra);
-
-    }
-
-});
+document.addEventListener("mousemove",(e)=>{
 
     cursorPlantio.style.left =
         e.clientX + "px";
@@ -450,16 +497,29 @@ document.addEventListener("mousemove", e => {
    ESC
 ================================== */
 
-document.addEventListener("keydown", (e) => {
-    if (e.key !== "Escape") return;
+document.addEventListener("keydown",(e)=>{
+
+    if(e.key !== "Escape") return;
 
     culturaSelecionada = null;
+
+    modoColheita = false;
+
     menuPlantio.classList.add("oculto");
+
+    balaoFoice.classList.add("oculto");
+
     cursorPlantio.classList.add("oculto");
+
     cursorFoice.classList.add("oculto");
 
-    document.body.classList.remove("plantio-ativo");
-    document.body.classList.remove("colheita-ativa");
+    document.body.classList.remove(
+        "plantio-ativo"
+    );
+
+    document.body.classList.remove(
+        "colheita-ativa"
+    );
 
 });
 
@@ -467,22 +527,41 @@ document.addEventListener("keydown", (e) => {
    CANCELAR CLICANDO FORA
 ================================== */
 
-document.addEventListener("click", (e) => {
+document.addEventListener("click",(e)=>{
 
-    const terra = e.target.closest(".terra");
+    const terra =
+        e.target.closest(".terra");
 
-    const menu = e.target.closest("#menuPlantio");
+    const menu =
+        e.target.closest("#menuPlantio");
 
-    if (terra || menu) return;
+    const balao =
+        e.target.closest("#balaoFoice");
+
+    if(
+        terra ||
+        menu ||
+        balao
+    ) return;
 
     culturaSelecionada = null;
 
+    modoColheita = false;
+
     menuPlantio.classList.add("oculto");
+
+    balaoFoice.classList.add("oculto");
 
     cursorPlantio.classList.add("oculto");
 
     cursorFoice.classList.add("oculto");
 
-    document.body.classList.remove("plantio-ativo");
+    document.body.classList.remove(
+        "plantio-ativo"
+    );
+
+    document.body.classList.remove(
+        "colheita-ativa"
+    );
 
 });
