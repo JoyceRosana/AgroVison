ff196fcd91b60b16a769b979a3338a7519f84bc7
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-13 02:38:03 -0300

    general update

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -1,4 +1,16 @@
-const plantacao = document.getElementById("plantacao");
+/* ==========================
+   REFERÊNCIAS
+========================== */
+
+const plantacao =
+document.getElementById("plantacao");
+
+const menuPlantio =
+document.getElementById("menuPlantio");
+
+/* ==========================
+   GRADE DE TERRAS
+========================== */
 
 const COLUNAS = 10;
 const LINHAS = 11;
@@ -10,29 +22,89 @@ for(let linha = 0; linha < LINHAS; linha++){
 
     for(let coluna = 0; coluna < COLUNAS; coluna++){
 
-        const terra = document.createElement("div");
+        const terra =
+        document.createElement("div");
 
         terra.className = "terra";
 
-        const img = document.createElement("img");
+        const img =
+        document.createElement("img");
 
-        img.src = "/img/simulador/terra.png";
+        img.src =
+        "/img/simulador/terra.png";
 
         terra.appendChild(img);
 
         const x =
-            (coluna * ESPACO_X) -
-            (linha * ESPACO_X);
+        (coluna * ESPACO_X) -
+        (linha * ESPACO_X);
 
         const y =
-            (coluna * ESPACO_Y) +
-            (linha * ESPACO_Y);
+        (coluna * ESPACO_Y) +
+        (linha * ESPACO_Y);
 
-        terra.style.left = x + "px";
-        terra.style.top = y + "px";
+        terra.style.left =
+        x + "px";
+
+        terra.style.top =
+        y + "px";
 
         plantacao.appendChild(terra);
 
     }
 
 }
+
+/* ==========================
+   ABRIR MENU
+========================== */
+
+plantacao.addEventListener(
+"click",
+(e)=>{
+
+    const terra =
+    e.target.closest(".terra");
+
+    if(!terra) return;
+
+    menuPlantio.style.left =
+    (e.clientX - 160) + "px";
+
+    menuPlantio.style.top =
+    (e.clientY - 220) + "px";
+
+    menuPlantio.classList.remove(
+        "oculto"
+    );
+
+}
+);
+
+/* ==========================
+   FECHAR MENU
+========================== */
+
+document.addEventListener(
+"click",
+(e)=>{
+
+    const terra =
+    e.target.closest(".terra");
+
+    const menu =
+    e.target.closest(
+        "#menuPlantio"
+    );
+
+    if(
+        terra ||
+        menu
+    ) return;
+
+    menuPlantio.classList.add(
+        "oculto"
+    );
+
+}
+);
