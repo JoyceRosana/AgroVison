ba65d215cd76e5d44f0405ae703f272e86ba3df3
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-13 22:02:54 -0300

    general add terra

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -3,6 +3,8 @@
 ========================== */
 
 const plantacao = document.getElementById("plantacao");
+const plantacao2 = document.getElementById("plantacao2");
+
 const menuPlantio = document.getElementById("menuPlantio");
 const viewport = document.querySelector(".viewport");
 
@@ -38,6 +40,35 @@ for (let linha = 0; linha < LINHAS; linha++) {
     terra.style.top = y + "px";
 
     plantacao.appendChild(terra);
+  }
+}
+
+function criarPlantacao(container) {
+  const COLUNAS = 10;
+  const LINHAS = 11;
+
+  const ESPACO_X = 50;
+  const ESPACO_Y = 25;
+
+  for (let linha = 0; linha < LINHAS; linha++) {
+    for (let coluna = 0; coluna < COLUNAS; coluna++) {
+
+      const terra = document.createElement("div");
+      terra.className = "terra";
+
+      const img = document.createElement("img");
+      img.src = "/img/simulador/terra.png";
+
+      terra.appendChild(img);
+
+      const x = (coluna * ESPACO_X) - (linha * ESPACO_X);
+      const y = (coluna * ESPACO_Y) + (linha * ESPACO_Y);
+
+      terra.style.left = x + "px";
+      terra.style.top = y + "px";
+
+      container.appendChild(terra);
+    }
   }
 }
 
