6ad73bf49f42599f2536950ab6bbc5466d7e5522
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-13 22:46:51 -0300

    platacoes update

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -20,7 +20,7 @@ const configuracoesPlantacao = {
   plantacao1: { linhas: 10, colunas: 10 },
   plantacao2: { linhas: 10, colunas: 7 },
   plantacao3: { linhas: 5, colunas: 4 },
-  plantacao4: { linhas: 5, colunas: 3 }
+  plantacao4: { linhas: 4, colunas: 3 }
 };
 
 /* ==========================
